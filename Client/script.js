d2a9bbdba6ad4949549872b8fe0c8e179e1d3efb
Gopal Shukla <gopalshukla2257@gmail.com>
2026-03-27 23:45:31 +0530

    Update socket connection URL to production server

--- Client/script.js
+++ Client/script.js
@@ -1,5 +1,4 @@
-const socket = io("http://localhost:3000");
-
+const socket = io("https://https://drunk-yard.onrender.com");
 socket.on("connect", () => {
   console.log("Connected:", socket.id);
 });
